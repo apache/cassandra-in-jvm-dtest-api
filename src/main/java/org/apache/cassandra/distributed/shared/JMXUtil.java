f3a3b982227602ec0752a5d0d4d780dc62ac3f6d
Doug Rohrer <682821+JeetKunDoug@users.noreply.github.com>
2023-08-11 10:46:44 -0400

    CASSANDRA-18727 - JMXUtil.getJmxConnector should retry connection attempts (#37)
    JMXUtil.getJmxConnector should retry connection attempts
    
    Patch by Doug Roher for CASSANDRA-18727. Reviewed by Jon Meredith and Francisco Guerrero.

--- src/main/java/org/apache/cassandra/distributed/shared/JMXUtil.java
+++ src/main/java/org/apache/cassandra/distributed/shared/JMXUtil.java
@@ -18,10 +18,15 @@
 
 package org.apache.cassandra.distributed.shared;
 
-import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.apache.cassandra.distributed.api.IInstanceConfig;
 
@@ -31,23 +36,92 @@ public final class JMXUtil
     {
     }
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(JMXUtil.class);
+
     public static final String JMX_SERVICE_URL_FMT = "service:jmx:rmi:///jndi/rmi://%s:%d/jmxrmi";
 
+    /**
+     * Create an instance of a {@link JMXConnector} to an in-jvm instance based on the input configuration.
+     * This overload uses 5 as the default number of retries which has been shown to be adequate in testing,
+     * and passes a null environment map to the connect call.
+     * @param config The instance configuration to use to get the necessary parameters to connect
+     * @return A JMXConnector instance which can communicate with the specified instance via JMX
+     */
     public static JMXConnector getJmxConnector(IInstanceConfig config) {
+        return getJmxConnector(config, 5, null);
+    }
+
+    /**
+     * Create an instance of a {@link JMXConnector} to an in-jvm instance based on the input configuration.
+     * This overload uses 5 as the default number of retries which has been shown to be adequate in testing.
+     * @param config The instance configuration to use to get the necessary parameters to connect
+     * @param jmxEnv an optional map which specifies the JMX environment to use. Can be null.
+     * @return A JMXConnector instance which can communicate with the specified instance via JMX
+     */
+    public static JMXConnector getJmxConnector(IInstanceConfig config, Map<String, ?> jmxEnv) {
+        return getJmxConnector(config, 5, jmxEnv);
+    }
+
+
+    /**
+     * Create an instance of a {@link JMXConnector} to an in-jvm instance based on the input configuration
+     * This overload passes a null environment map to the connect call.
+     * @param config The instance configuration to use to get the necessary parameters to connect
+     * @param numAttempts the number of retries to attempt before failing to connect.
+     * @return A JMXConnector instance which can communicate with the specified instance via JMX
+     */
+    public static JMXConnector getJmxConnector(IInstanceConfig config, int numAttempts)
+    {
+        return getJmxConnector(config, numAttempts, null);
+    }
+
+    /**
+     * Create an instance of a {@link JMXConnector} to an in-jvm instance based on the input configuration
+     * @param config The instance configuration to use to get the necessary parameters to connect
+     * @param numAttempts the number of retries to attempt before failing to connect.
+     * @param jmxEnv an optional map which specifies the JMX environment to use. Can be null.
+     * @return A JMXConnector instance which can communicate with the specified instance via JMX
+     */
+    public static JMXConnector getJmxConnector(IInstanceConfig config, int numAttempts, Map<String, ?> jmxEnv) {
         String jmxHost = getJmxHost(config);
         String url = String.format(JMX_SERVICE_URL_FMT, jmxHost, config.jmxPort());
-        try
+        int attempts = 1;
+        Throwable lastThrown = null;
+        while (attempts <= numAttempts)
         {
-            return JMXConnectorFactory.connect(new JMXServiceURL(url), null);
+            try
+            {
+                JMXConnector connector = JMXConnectorFactory.connect(new JMXServiceURL(url), jmxEnv);
+
+                LOGGER.info("Connected to JMX server at {} after {} attempt(s)",
+                            url, attempts);
+                return connector;
+            }
+
+            catch(MalformedURLException e)
+            {
+                // Because we are building the URL from the config, this should never happen.
+                // This is unrecoverable, so just rethrow wrapped in RuntimeException
+                throw new RuntimeException("Failed to connect to JXM server due to a malformed URL (Instance config must be broken?)", e);
+            }
+
+            catch (Throwable thrown)
+            {
+                lastThrown = thrown;
+            }
+            if (attempts < numAttempts)
+            {
+                LOGGER.info("Could not connect to JMX on {} after {} attempts. Will retry.", url, attempts);
+                Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS);
+            }
+            attempts++;
         }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
+        String error = "Failed to connect to JMX, which was unreachable after " + attempts + " attempts.";
+        LOGGER.error(error, lastThrown);
+        throw new RuntimeException(error, lastThrown);
     }
 
     public static String getJmxHost(IInstanceConfig config) {
         return config.broadcastAddress().getAddress().getHostAddress();
     }
-
 }
